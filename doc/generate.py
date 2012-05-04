0acfd030b370f292f183b6ce42f76280284cc7ae
Pekka Klärck <devnull@localhost>
2012-05-04 15:16:25 +0300

    use libdoc integrated with rf 2.7+

--- doc/generate.py
+++ doc/generate.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 from os.path import abspath, dirname, join
-from libdoc import LibraryDoc, create_html_doc
+from robot.libdoc import libdoc
 
 docdir = dirname(abspath(__file__))
 libpath = join(docdir, '..', 'src', 'SeleniumLibrary')
 outpath = join(docdir, 'SeleniumLibrary.html')
 
-lib = LibraryDoc(libpath)
-create_html_doc(lib, outpath)
-print lib.name, lib.version
-print outpath
+libdoc(libpath, outpath)
+
